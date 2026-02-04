45e728221cf490471510747bbfa26e203d79abb7
diasturdaly <diass071109@icloud.com>
2026-02-04 21:38:20 +0500

    Assignment 4

---
+++ src/kz/movie/UpdatePriceRequest.java
@@ -0,0 +1,10 @@
+package kz.movie;
+
+public class UpdatePriceRequest {
+    private int price;
+
+    public UpdatePriceRequest() {}
+
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+}

b0454ca326b2f209bf9d9564c3d78e3b16543093
diasturdaly <diass071109@icloud.com>
2026-02-04 21:53:19 +0500

    Assignment 4

--- src/kz/movie/FilmController.java
+++ src/kz/movie/FilmController.java
@@ -1,21 +1,34 @@
 package kz.movie;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/films")
 public class FilmController {
 
-    // Біздің базамен жұмыс істейтін репозиторийіміз
     private final FilmRepository filmRepository = new FilmRepository();
 
     @GetMapping
-    public List<Film> getAllFilms() {
-        // Базадан барлық фильмдерді алып, тізім ретінде қайтарамыз
-        // Spring оны автоматты түрде JSON қылып береді
+    public List<Film> getFilms() {
         return filmRepository.getAllFilms();
+    }
+
+    @PostMapping
+    public String add(@RequestBody Film film) {
+        filmRepository.addFilm(film);
+        return "The film added!";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable int id, @RequestBody UpdatePriceRequest request) {
+        filmRepository.updatePrice(id, request.getPrice());
+        return "ID " + id + " Price updated!";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id) {
+        filmRepository.deleteFilm(id);
+        return "ID " + id + " film deleted!";
     }
 }
--- src/kz/movie/Main.java
+++ src/kz/movie/Main.java
@@ -1,21 +1,16 @@
-package kz.movie; // Папканың аты осылай болуы керек
+package kz.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// Мұнда біз Spring-ке базаны автоматты түрде қоспа дедік (exclude),
-// өйткені сенде өз бетіңше жазылған DbConnection.java бар.
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
-        // Бағдарламаны іске қосу
+
         SpringApplication.run(Main.class, args);
 
-        System.out.println("\n=======================================");
-        System.out.println("СЕРВЕР СӘТТІ ІСКЕ ҚОСЫЛДЫ!");
-        System.out.println("Мәліметтерді мына жерден қараңыз:");
+        System.out.println("Server started!");
         System.out.println("http://localhost:8080/api/films");
-        System.out.println("=======================================");
     }
 }
